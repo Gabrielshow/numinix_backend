0a33f940b934df569984462ee5e4116516f98ff5
Gabriel <128767774+Gabrielshow@users.noreply.github.com>
2024-10-29 15:17:58 +0100

    Update fix_categories.php

--- cron/fix_categories.php
+++ cron/fix_categories.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-      require('../includes/configure.php');
+    require('../includes/configure.php');
     ini_set('include_path', DIR_FS_CATALOG . PATH_SEPARATOR . ini_get('include_path'));
     chdir(DIR_FS_CATALOG);
     require_once('includes/application_top.php');
